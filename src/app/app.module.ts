fc1eea3b91d1fb6aa03dcaee05d2885906e49c2d
Salvasgdo <A01421806@itesm.mx>
2020-05-15 11:16:28 -0500

    No se

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieGridComponent } from './movie-grid/movie-grid.component';
 import { MovieHomeComponent } from './movie-home/movie-home.component';
+import { LoginComponent } from './User/login/login.component';
+import { RegisterComponent } from './User/register/register.component';
+import { SecondviewComponent } from './SecondView/secondview/secondview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MovieGridComponent,
-    MovieHomeComponent
+    MovieHomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    SecondviewComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
