9651e267118640f6799376954e61fc41208d9c07
LiraNeon <55103410+LiraNeon@users.noreply.github.com>
2020-05-21 19:31:17 -0500

    Pase todo al imdb.js
    pase todo al imdb.js

--- server/app.js
+++ server/app.js
@@ -3,17 +3,19 @@ var path= require('path');
 var bodyParser= require('body-parser');
 
 var apiIMDb= require('./routes/imdb');
+/*
 var apiPeliculas = require('./routes/peliculas');
 var apiPersonas = require('./routes/personas');
-
+*/
 var port= 3000;
 var app = express();
 
 app.use(express.json());
 app.use('/api', apiIMDb);
+/*
 app.use('/apiPelicula', apiPeliculas);
 app.use('/apiPersona', apiPersonas);
-
+*/
 
 app.listen(port, () => {
     console.log('Servidor iniciado en el puerto '+ port);
--- server/routes/imdb.js
+++ server/routes/imdb.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var usuariosController = require('../controllers/usuarios.controller');
+var peliculasController = require('../controllers/peliculas.controller');
+var personasController = require('../controllers/personas.controller');
+
 
 //Funciona como un front controller
 /*
@@ -11,10 +14,17 @@ router.put('/cars', carController.car_update);
 router.post('/cars', carController.car_save);
 router.delete('/cars/:id', carController.car_delete);
 */
+//Usuarios
 router.get('/usuarios', usuariosController.usuarios_list);
 
 
+//Peliculas
+router.get('/peliculas', peliculasController.peliculasList);
+router.get('/peliculas/peliculasGenero', peliculasController.peliculasByGenero);
+router.get('/peliculas/peliculasClasificacion', peliculasController.peliculasByClasificacion);
 
+//Personas
+router.get('/personas/aleatorio', personasController.personasAleatorias);
 
 
 module.exports = router;
