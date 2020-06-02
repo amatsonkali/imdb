dcb969fd6316a6f2f9aed07ab745b0bdf9207db5
k-atara <A01423052@itesm.mx>
2020-06-02 02:34:12 -0500

    rfg

--- src/app/movie/movie-calif/movie-calif.component.ts
+++ src/app/movie/movie-calif/movie-calif.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {Calificacion} from 'src/app/models/calificacion';
 import { MovieService } from '../movie-service/movie.service';
-import Swal from 'sweetalert2';
 import { AuthenticationService } from 'src/app/User/authentication.service';
 
 @Component({
@@ -13,7 +12,6 @@ export class MovieCalifComponent implements OnInit {
 
   idUsuario = Number(localStorage.getItem('usuario'));
 
-  califU : boolean;
   calificaciones : Calificacion[];
   calificacionUsuario: Calificacion = {
     calificacion : 0,
@@ -35,38 +33,16 @@ export class MovieCalifComponent implements OnInit {
 
   saveCalificacion(calificacionUsuario: Calificacion){
     console.log(this.calificacionUsuario);
-    for(let i=0; i<this.calificaciones.length; i++){
-      if(this.calificaciones[i].idUsuario == calificacionUsuario.idUsuario){
-        this.califU = true;
-      }
-    };
-
-    console.log(this.califU);
-    if(this.califU){
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Ya calificaste esta película. Consulte al administrador para más información',
-      })
-    }else{
-      if(calificacionUsuario.calificacion==0 || calificacionUsuario.subtitulo == ''){
-        Swal.fire({
-          icon: 'info',
-          title: 'Error',
-          text: 'Ingresa los datos completos',
-        })
-      }else{
-        this.movieService.saveCalificacion(calificacionUsuario).subscribe(data =>{
-          this.movieService.getCalificacionesById().subscribe((calificaciones: Calificacion[]) =>{
-            console.log(calificaciones);
-            this.calificaciones = calificaciones;
-            this.movieService.getSelectedPeli();
-          });
-          calificacionUsuario.calificacion = 0;
-          calificacionUsuario.subtitulo = '';
-          calificacionUsuario.comentario = '';
-        });
-      }
-    }
+    this.movieService.saveCalificacion(calificacionUsuario).subscribe(data =>{
+      this.movieService.getCalificacionesById().subscribe((calificaciones: Calificacion[]) =>{
+        console.log(calificaciones);
+        this.calificaciones = calificaciones;
+        this.movieService.getSelectedPeli();
+      });
+      calificacionUsuario.calificacion = 0;
+      calificacionUsuario.subtitulo = '';
+      calificacionUsuario.comentario = '';
+    });
   }
+
 }
